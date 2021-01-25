791897b2e8f204c21ede70b6d71600d4d48658b0
kmilc <klaudia.milc@wur.nl>
2021-01-25 14:44:23 +0100

    added documentation to the are funciton

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
+
 print('Hello world')
 print('Hi')
 
 import numpy as np
 
 def area(radius):
+"""
+this function calculates the area of a cricle with radius r
+"""
         a = np.pi*radius**2
         return(a)
 
