e6bcaf5e2bdf40ff25b716b172809d18d4796847
kmilc <klaudia.milc@wur.nl>
2021-01-25 14:40:41 +0100

    modiefied the files

--- script.py
+++ script.py
@@ -1,1 +1,3 @@
 print('this is a script')
+
+area = test.area(10)
--- test.py
+++ test.py
@@ -6,9 +6,6 @@ import numpy as np
 def area(radius):
         a = np.pi*radius**2
         return(a)
-    
-a = area(10)
-print(a)
 
 def circumference(radius):
 	c=2*np.pi*radius
